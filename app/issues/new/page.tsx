c944c05369c63d5b057f8748aa00189d4a1c691a
007saki <saki.baleiwai.komai@gmail.coom>
2025-02-17 13:30:44 +1200

    prisma install and configure and create models

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,50 @@
+
+
+
+import { Table } from '@radix-ui/themes'
+import React from 'react'
+
+const IssueNewPage = () => {
+
+    const IssueTable=()=>{
+        return (
+            <div>
+                <Table.Root>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.ColumnHeaderCell>Group</Table.ColumnHeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        <Table.Row>
+                            <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
+                            <Table.Cell>danilo@example.com</Table.Cell>
+                            <Table.Cell>Developer</Table.Cell>
+                        </Table.Row>
+
+                        <Table.Row>
+                            <Table.RowHeaderCell>Zahra Ambessa</Table.RowHeaderCell>
+                            <Table.Cell>zahra@example.com</Table.Cell>
+                            <Table.Cell>Admin</Table.Cell>
+                        </Table.Row>
+
+                        <Table.Row>
+                            <Table.RowHeaderCell>Jasper Eriksson</Table.RowHeaderCell>
+                            <Table.Cell>jasper@example.com</Table.Cell>
+                            <Table.Cell>Developer</Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table.Root>
+            </div>
+        )
+    }
+  return (
+    <div>
+        show issues using table
+        <IssueTable/>
+    </div>
+  )
+}
+
+export default IssueNewPage
