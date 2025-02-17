ffc6963ba2d5ad41e38399a1cc276ebaeb9d5d3f
007saki <saki.baleiwai.komai@gmail.coom>
2025-02-17 14:11:56 +1200

    install react hook form

--- app/issues/_component/IssueForm.tsx
+++ app/issues/_component/IssueForm.tsx
@@ -5,17 +5,29 @@
 import { Button, TextField } from '@radix-ui/themes'
 import "easymde/dist/easymde.min.css";
 import dynamic from 'next/dynamic';
+import { Controller, useForm } from 'react-hook-form';
 
 const SimpleMDE = dynamic(()=>import('react-simplemde-editor'),{ssr:false})
 
+interface Props{
+    title:string,
+    description:string,
+}
 
 const IssueForm = () => {
+    const {register,control, handleSubmit} = useForm<Props>()
   return (
-    <div className='max-w-lg space-y-5'>
-        <TextField.Root variant='soft' color='purple' placeholder='Enter Issue'/>
-        <SimpleMDE />
+    <form onSubmit={handleSubmit((data)=>console.log(data))} className='max-w-lg space-y-5'>
+        <TextField.Root {...register('title')} variant='soft' color='purple' placeholder='Enter Issue'/>
+        
+        <Controller
+        control={control}
+        name='description'
+        render={({field})=><SimpleMDE{...field} placeholder='Enter Description' />}
+        />
+        
         <Button>Add New Issue</Button>
-    </div>
+    </form>
   )
 }
 
