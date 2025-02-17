4d8a9d38cd7f9ed0fd75a05bc6182bb0726ac1ce
007saki <saki.baleiwai.komai@gmail.coom>
2025-02-17 16:02:03 +1200

    created issue view

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,16 +1,46 @@
 
 
 
-import { Button } from '@radix-ui/themes'
+import { prisma } from '@/prisma/client'
+import { Button, Table } from '@radix-ui/themes'
 import Link from 'next/link'
 import React from 'react'
 
 const IssuesPage = () => {
+
+
+  const IssueTable=async()=>{
+    const issues = await prisma.issue.findMany()
+    return(
+      <div>
+        <Table.Root variant='surface'>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Issues</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {issues.map(issue=>
+            <Table.Row key={issue.id}>
+              <Table.Cell>{issue.title}</Table.Cell>
+              <Table.Cell>{issue.status}</Table.Cell>
+              <Table.Cell>{issue.createdAt.toDateString()}</Table.Cell>
+            </Table.Row>
+            )}
+
+          </Table.Body>
+        </Table.Root>
+      </div>
+    )
+  }
   return (
-    <div>
+    <div className='space-y-5'>
         <Button>
             <Link href={'/issues/new'}>Add New Issue</Link>
         </Button>
+        <IssueTable />
     </div>
   )
 }
