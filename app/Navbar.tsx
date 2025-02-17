4f741dc789200a9bc984d68c0b7dab787c729119
007saki <saki.baleiwai.komai@gmail.coom>
2025-02-17 13:01:27 +1200

    style nav link

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,17 +1,33 @@
 
-
+'use client'
+import classNames from 'classnames'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { FaBug } from 'react-icons/fa'
 
 const Navbar = () => {
+  const currentPath = usePathname()
+  const links = [
+    {href:'/', label:'Dashboard'},
+    {href:'/issues', label:'Issues'},
+  ]
   return (
     <nav className={'flex gap-5 px-5 items-center h-10 border-b mb-5'}>
         <Link href={'/'}><FaBug color='purple'/></Link>
-        <ul className={'flex gap-5'}>
-            <li><Link href={'/'}>Dashboard</Link></li>
-            <li><Link href={'/Issues'}>Issues</Link></li>
-        </ul>
+        {links.map(link=>
+          <ul key={link.href}>
+            <li><Link
+            className={classNames({
+              'text-zinc-900':currentPath===link.href,
+              'text-zinc-500':currentPath!==link.href,
+              'transition-colors':true,
+              'hover:text-zinc-900':true,
+              
+            })}
+            href={link.href}>{link.label}</Link></li>
+          </ul>
+        )}
     </nav>
   )
 }
