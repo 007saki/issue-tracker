93d6df503c2c47ff6bd672f49829bb1fe9bce139
007saki <saki.baleiwai.komai@gmail.coom>
2025-02-19 11:57:57 +1200

    change layout navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -17,21 +17,23 @@ const Navbar = () => {
   ]
 
   return (
-    <nav className={'flex gap-5 px-5 items-center h-10 border-b mb-5'}>
-        <Link href={'/'}><FaBug color='purple'/></Link>
-        {links.map(link=>
-          <ul key={link.href}>
-            <li><Link
-            className={classNames({
-              'text-zinc-900':currentPath===link.href,
-              'text-zinc-500':currentPath!==link.href,
-              'transition-colors':true,
-              'hover:text-zinc-900':true,
-            })}
-            href={link.href}>{link.label}</Link></li>
-          </ul>
-        )}
-        <Box>
+    <nav className={'flex gap-5 px-5 items-center h-10 border-b mb-5 justify-between'}>
+        <Box className='flex gap-5 items-center'>
+          <Link href={'/'}><FaBug color='purple'/></Link>
+          {links.map(link=>
+            <ul key={link.href}>
+              <li><Link
+              className={classNames({
+                'text-zinc-900':currentPath===link.href,
+                'text-zinc-500':currentPath!==link.href,
+                'transition-colors':true,
+                'hover:text-zinc-900':true,
+              })}
+              href={link.href}>{link.label}</Link></li>
+            </ul>
+          )}
+        </Box>
+        <Box className='px-10'>
           {status==='authenticated'&&<Link href={'/api/auth/signout'}>Logout</Link>}
           {status==='unauthenticated'&&<Link href={'/api/auth/signin'}>Login</Link>}
         </Box>
